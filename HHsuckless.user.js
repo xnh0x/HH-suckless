f6245d300655e9aabf643a9ed42652bbf24c2677
xnh0x <xnh0x@pm.me>
2025-06-18 04:00:42 +0200

    fix shop not opening on first click of the plus button at the top

--- HHsuckless.user.js
+++ HHsuckless.user.js
@@ -297,14 +297,16 @@ const local_now_ts = Math.floor(Date.now() / 1000);
     }
 
     function home() {
-        preventAutoPopup('.info-container .chest-container', '#shop-payment-tabs', '#common-popups close');
-        preventAutoPopup('#news_button', '#news_details_popup', '#common-popups close');
+        preventAutoPopup(['.info-container .chest-container', '.currency plus'], '#shop-payment-tabs', '#common-popups close');
+        preventAutoPopup(['#news_button'], '#news_details_popup', '#common-popups close');
 
-        function preventAutoPopup(button, check, close) {
+        function preventAutoPopup(manualButtons, check, close) {
             let manualClick = false;
-            $(button).on('click', () => {
-                manualClick = true;
-            });
+            for (const button of manualButtons) {
+                $(button).on('click', () => {
+                    manualClick = true;
+                });
+            }
             HHPlusPlus.Helpers.doWhenSelectorAvailable(check, ()=>{
                 if (!manualClick) {
                     $(close).trigger('click');
