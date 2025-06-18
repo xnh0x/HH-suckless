af1a1d01d2398632f7e2d9147ec9cc8dd89dbe66
xnh0x <xnh0x@pm.me>
2025-06-18 03:14:11 +0200

    version 0.8

--- HHsuckless.user.js
+++ HHsuckless.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HH suckless
-// @version      0.7
+// @version      0.8
 // @description  HH that sucks less. Requires HH++ BDSM and Rena's Battle Sim.
 // @author       xnh0x
 // @match        https://*.hentaiheroes.com/*
